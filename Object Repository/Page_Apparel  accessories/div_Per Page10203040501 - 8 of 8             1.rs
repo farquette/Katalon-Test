d72b8dac5bf6ead889b9a5d62b4e2c00c4bab250
FrancisArquette <FrancisArquette@PQA-0838LT>
2024-12-03 13:12:19 -0500

    Week 2 day 2

---
+++ Object Repository/Page_Apparel  accessories/div_Per Page10203040501 - 8 of 8             1.rs
@@ -0,0 +1,171 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Per Page10203040501 - 8 of 8             1</name>
+   <tag></tag>
+   <elementGuidId>661c1137-f85c-4134-b091-1ba6c3e65b9e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='maincontainer']/div/div/div/div/div[4]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>20f0aad5-357c-4594-861d-d3d006effccd</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>sorting well</value>
+      <webElementGuid>94c23b7e-24fa-4cc8-8d28-fb91d08a952f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+		
+
+Per Page  
+
+			10
+			20
+			30
+			40
+			50
+	
+  
+1 - 8 of 8
+
+
+    
+  	  	 
+    	1
+  	 
+  	  	
+
+
+		
+		
+	</value>
+      <webElementGuid>0a69b88c-90d9-4a17-b1cd-6b6d2edee6fd</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;maincontainer&quot;)/div[@class=&quot;container-fluid&quot;]/div[@class=&quot;col-md-12 col-xs-12 mt20&quot;]/div[1]/div[@class=&quot;contentpanel&quot;]/div[@class=&quot;sorting well&quot;]</value>
+      <webElementGuid>e64dab0f-0081-44f7-8251-2ac20a513f65</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='maincontainer']/div/div/div/div/div[4]</value>
+      <webElementGuid>971d0a0a-c918-4dd6-965f-ed843a4fab98</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='$19.06'])[4]/following::div[1]</value>
+      <webElementGuid>6ad18a55-5433-478f-aa7d-3a42f2490e83</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='View'])[16]/following::div[5]</value>
+      <webElementGuid>e898a641-ec54-4811-bc77-4aca19ecd5f3</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Contact Us'])[1]/preceding::div[4]</value>
+      <webElementGuid>00b2923c-c119-4725-872a-96139a80fdb0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div/div/div/div[4]</value>
+      <webElementGuid>98ad7ae6-efae-4156-887f-e55ad5e4c7b1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+		
+
+Per Page  
+
+			10
+			20
+			30
+			40
+			50
+	
+  
+1 - 8 of 8
+
+
+    
+  	  	 
+    	1
+  	 
+  	  	
+
+
+		
+		
+	' or . = '
+		
+
+Per Page  
+
+			10
+			20
+			30
+			40
+			50
+	
+  
+1 - 8 of 8
+
+
+    
+  	  	 
+    	1
+  	 
+  	  	
+
+
+		
+		
+	')]</value>
+      <webElementGuid>220ec090-42da-4bdb-91cf-1bdb9317008c</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Page_Apparel  accessories/ul_T-shirts.rs
@@ -0,0 +1,116 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ul_T-shirts</name>
+   <tag></tag>
+   <elementGuidId>8d6e656d-be3e-4e2d-8cb7-7c83ddabcf74</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='maincontainer']/div/div/div/div/ul</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>ul.thumbnails.row</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>ul</value>
+      <webElementGuid>28aeda3d-8507-461f-ba27-5377399cf8d9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>thumbnails row</value>
+      <webElementGuid>ae4bcdd3-9a9d-4188-889e-93a82ac54f26</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+	    	     
+	    	
+	    			    	
+	    	
+	    	T-shirts
+	    	
+	    
+	    	</value>
+      <webElementGuid>0f9813dc-20fb-4b63-aaeb-03d46c2acf5c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;maincontainer&quot;)/div[@class=&quot;container-fluid&quot;]/div[@class=&quot;col-md-12 col-xs-12 mt20&quot;]/div[1]/div[@class=&quot;contentpanel&quot;]/ul[@class=&quot;thumbnails row&quot;]</value>
+      <webElementGuid>0a0dcb22-e499-4524-b962-091f563f6991</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='maincontainer']/div/div/div/div/ul</value>
+      <webElementGuid>a8350496-b836-45e9-a75d-d7d1b6bb3145</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Apparel &amp; accessories'])[4]/following::ul[1]</value>
+      <webElementGuid>5f7375c7-d547-44d9-81e9-678610b77020</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Apparel &amp; accessories'])[3]/following::ul[1]</value>
+      <webElementGuid>038b3a47-c573-4ada-9c34-88d569ab283c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div/div/div/ul</value>
+      <webElementGuid>12aa359e-56d8-4884-9e56-c6472e787cf7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//ul[(text() = '
+	    	     
+	    	
+	    			    	
+	    	
+	    	T-shirts
+	    	
+	    
+	    	' or . = '
+	    	     
+	    	
+	    			    	
+	    	
+	    	T-shirts
+	    	
+	    
+	    	')]</value>
+      <webElementGuid>590d5134-e924-4bc7-8019-580ff4481423</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
